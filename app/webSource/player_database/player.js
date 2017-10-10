632b004f5acbfd76ecb7e38d1c982ba35b2173e8
sanaehururu1200 <sanaehururu1200@gmail.com>
2017-10-10 16:33:15 +0900

    権限の調整とfetchの使用

--- app/webSource/player_database/player.js
+++ app/webSource/player_database/player.js
@@ -1,20 +1,15 @@
 $(function(){
   if(location.search.split('=')[0] === '?name' && location.search.split('=')[1]){
     const url = 'http://api.otos.red/players/' + location.search.split('=')[1] + '.json'; // リクエスト先URL
-    let request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.onreadystatechange = function () {
-      if (request.readyState != 4) {
-        document.getElementById('title').innerText = 'Now Loading...';
-      } else if (request.status != 200) {
-        document.getElementById('title').innerHTML = '接続失敗 <br> サーバーが落ちているかユーザーが存在しません';
-      } else {
-        let json = JSON.parse(request.responseText);
+    fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(json) {
         document.getElementById('title').innerText = location.search.split('=')[1];
           //br
           var br = document.createElement("br");
           document.body.appendChild(br);
-          //bedwars['level']
+
+          //bedwars->level
           var card = document.createElement("div");
           card.classList.add('uk-card-default','uk-card-body','uk-width-1-2@s','uk-card-hover');
           document.body.appendChild(card);
@@ -28,7 +23,8 @@ $(function(){
           card.appendChild(content);
           var br = document.createElement("br");
           document.body.appendChild(br);
-          //bedwars['exp']
+
+          //bedwars->exp
           var card = document.createElement("div");
           card.classList.add('uk-card-default','uk-card-body','uk-width-1-2@s','uk-card-hover');
           document.body.appendChild(card);
@@ -40,10 +36,10 @@ $(function(){
           content.classList.add('uk-card-title');
           content.innerText = json['bedwars']['exp'];
           card.appendChild(content);
+
+          //br
           var br = document.createElement("br");
           document.body.appendChild(br);
-      }
-    };
-    request.send(null);
+    })
   }
 });
