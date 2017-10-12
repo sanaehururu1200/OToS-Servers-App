93dc93c0c41a4584314d381850737f11477f4e0c
sanaehururu1200 <sanaehururu1200@gmail.com>
2017-10-12 16:02:32 +0900

    無駄な改行の削除

--- app/src/main/java/red/otos/otos_servers_app/TopActivity.java
+++ app/src/main/java/red/otos/otos_servers_app/TopActivity.java
@@ -11,7 +11,7 @@ import android.webkit.WebViewClient;
 
 
 public class TopActivity extends AppCompatActivity {
-    
+
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
